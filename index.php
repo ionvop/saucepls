80c37a68656a4f3115bb413a1c92e2103f92b36f
ionvop <43801264+ionvop@users.noreply.github.com>
2023-09-27 07:29:28 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+include("common.php");
+Debug();
+
+?>
+
+<html>
+    <head>
+        <title>
+            SaucePls
+        </title>
+        <base href="./">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <link rel="stylesheet" href="style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <div class="main">
+            <?=SetHeader()?>
+        </div>
+    </body>
+    <script src="script.js"></script>
+</html>
