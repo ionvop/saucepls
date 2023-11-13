62337df6e75a4e67699934e40e7b384557bf5b3b
ionvop <ionvop@gmail.com>
2023-11-13 09:45:06 +0800

    Update

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ function btnLogout() {
 function EraseCookie(name) {   
     document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
+
+for (let element of document.querySelectorAll(".-script__textarea--dynamic")) {
+    element.oninput = () => {
+        element.style.resize = "none";
+        element.style.height = "";
+        element.style.height = element.scrollHeight + "px"
+    }
+}
