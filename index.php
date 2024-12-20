5884634d6d9c306ccb6a01eb26f0c4a977ada41c
ionvop <ionvop@gmail.com>
2024-12-21 00:16:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,3 +4,27 @@ include("common.php");
 Debug();
 
 ?>
+
+<html>
+    <head>
+        <title>
+            SaucePls
+        </title>
+        <base href="./">
+        <link rel="stylesheet" href="style.css">
+        <link rel="icon" href="favicon.ico">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+
+        </style>
+    </head>
+    <body>
+        <div class="main">
+            <?=SetHeader()?>
+        </div>
+    </body>
+    <script src="script.js"></script>
+    <script>
+
+    </script>
+</html>
