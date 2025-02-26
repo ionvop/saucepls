428e4ecba949bf4454787728d1b5944cc0b48906
ionvop <ionvop@gmail.com>
2025-02-26 23:08:21 +0800

    Update server.php

--- server.php
+++ server.php
@@ -33,14 +33,16 @@ if (isset($_POST["method"])) {
 
 function Login() {
     global $BREVO_API_KEY;
+
+    if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) == false) {
+        Alert("Invalid email.");
+    }
+
     $code = substr(md5(time()), 0, 5);
     session_start();
 
-    $_SESSION["verify"] = [
-        "email" => $_POST["email"],
-        "code" => $code,
-        "expiry" => time() + 300
-    ];
+    $_SESSION["code"] = $code;
+    $_SESSION["email"] = $_POST["email"];
 
     $headers = [
         "Content-Type: application/json",
@@ -62,53 +64,128 @@ function Login() {
         "subject" => "SaucePls login code"
     ];
 
+    echo <<<HTML
+        <h1>{$body["subject"]}</h1>
+        <pre>{$body["textContent"]}</pre><br><br>
+        <a href="login/verify/">
+            <button>
+                Continue
+            </button>
+        </a>
+    HTML;
+
+    exit();
+
     SendCurl("https://api.sendinblue.com/v3/smtp/email", "POST", $headers, json_encode($body));
     header("Location: login/verify/");
     exit();
 }
 
 function Verify() {
-    $data = GetSiteData();
+    $db = new SQLite3("database.db");
     session_start();
 
-    if (time() > $_SESSION["verify"]["expiry"]) {
-        Alert("Your code is incorrect or may have expired.");
+    if ($_POST["code"] != $_SESSION["code"]) {
+        Alert("Incorrect code.");
     }
 
-    if ($_POST["code"] != $_SESSION["verify"]["code"]) {
-        Alert("Your code is incorrect or may have expired.");
-    }
+    $query = <<<SQL
+        SELECT * FROM `users` WHERE `email` = :email
+    SQL;
 
-    $userIndex = FindIndex($data["users"], "email", $_SESSION["verify"]["email"]);
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(":email", $_SESSION["email"]);
+    $user = $stmt->execute()->fetchArray(SQLITE3_ASSOC);
 
-    if ($userIndex == -1) {
+    if ($user == false) {
         header("Location: register/");
         exit();
     }
 
-    $user = $data["users"][$userIndex];
-    $sessionId = NewSession($user["id"]);
-    
-    if ($sessionId == false) {
-        Alert("There was an error logging you in.");
+    session_destroy();
+    $session = uniqid("session-");
+
+    $query = <<<SQL
+        UPDATE `users` SET `session` = :session WHERE `id` = :id
+    SQL;
+
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(":session", $session);
+    $stmt->bindValue(":id", $user["id"]);
+    $stmt->execute();
+    setcookie("session", $session, time() + (86400 * 30));
+    header("Location: ./");
+}
+
+function Register() {
+    $db = new SQLite3("database.db");
+
+    if (strlen($_POST["username"]) < 4) {
+        Alert("Your username must be at least 4 characters long.");
     }
 
-    $_SESSION["verify"] = null;
-    setcookie("session", $sessionId, time() + 86400);
+    if (strlen($_POST["username"]) > 20) {
+        Alert("Your username must be less than 20 characters long.");
+    }
+
+    if (preg_match("/[^a-zA-Z0-9-_]/", $_POST["username"])) {
+        Alert("Your username can only contain letters, numbers, underscores, and hyphens.");
+    }
+
+    session_start();
+
+    $query = <<<SQL
+        SELECT * FROM `users` WHERE `username` = :username OR `email` = :email
+    SQL;
+
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(":username", $_POST["username"]);
+    $stmt->bindValue(":email", $_SESSION["email"]);
+    $user = $stmt->execute()->fetchArray(SQLITE3_ASSOC);
+
+    if ($user != false) {
+        Alert("That username is already taken.");
+    }
+
+    $session = uniqid("session-");
+
+    $query = <<<SQL
+        INSERT INTO `users` (`username`, `email`, `session`) VALUES (:username, :email, :session)
+    SQL;
+
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(":username", $_POST["username"]);
+    $stmt->bindValue(":email", $_SESSION["email"]);
+    $stmt->bindValue(":session", $session);
+    $stmt->execute();
+    session_destroy();
+    setcookie("session", $session, time() + (86400 * 30));
     header("Location: ./");
     exit();
 }
 
-function Register() {
-    $data = GetSiteData();
-    session_start();
+function Logout() {
+    $db = new SQLite3("database.db");
+    $user = GetUser();
 
-    if (filter_var($_SESSION["verify"]["email"], FILTER_VALIDATE_EMAIL) == false) {
-        Alert("That email is invalid.");
-    }
+    $query = <<<SQL
+        UPDATE `users` SET `session` = NULL WHERE `id` = :id
+    SQL;
 
-    if (FindIndex($data["users"], "email", $_SESSION["verify"]["email"]) != -1) {
-        Alert("That email is already registered.");
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(":id", $user["id"]);
+    $stmt->execute();
+    setcookie("session", "", time() - 3600);
+    header("Location: ./");
+    exit();
+}
+
+function EditProfile() {
+    $db = new SQLite3("database.db");
+    $user = GetUser();
+
+    if ($user == false) {
+        Alert("Unauthorized.");
     }
 
     if (strlen($_POST["username"]) < 4) {
@@ -119,85 +196,35 @@ function Register() {
         Alert("Your username must be less than 20 characters long.");
     }
 
-    if (preg_match("/^[a-zA-Z0-9_-]+$/", $_POST["username"]) == 0) {
+    if (preg_match("/[^a-zA-Z0-9-_]/", $_POST["username"])) {
         Alert("Your username can only contain letters, numbers, underscores, and hyphens.");
     }
 
-    if (FindIndex($data["users"], "username", $_POST["username"]) != -1) {
-        Alert("That username is already taken.");
+    if (strlen($_POST["description"]) > 10000) {
+        Alert("Your description must be less than 10000 characters long.");
     }
-
-    $user = [
-        "id" => uniqid("user"),
-        "username" => $_POST["username"],
-        "email" => $_SESSION["verify"]["email"],
-        "avatar" => "default.jpg",
-        "description" => "Hello, world!",
-        "type" => "member",
-        "lastSeen" => time(),
-        "time" => time()
-    ];
-
-    $data["users"][] = $user;
-
-    if (SetSiteData($data) == false) {
-        Alert("There was an error registering your account.");
-    }
-
-    $sessionId = NewSession($user["id"]);
-
-    if ($sessionId == false) {
-        Alert("There was an error logging you in.");
-    }
-
-    $_SESSION["verify"] = null;
-    setcookie("session", $sessionId, time() + 86400);
-    header("Location: ./");
-    exit();
-}
-
-function Logout() {
-    $data = GetSiteData();
-
-    $data["sessions"] = array_values(array_filter($data["sessions"], function ($session) use ($data) {
-        if ($session["id"] == $_COOKIE["session"]) {
-            return false;
-        }
-
-        return true;
-    }));
-
-    if (SetSiteData($data) == false) {
-        Alert("There was an error logging you out.");
-    }
-
-    setcookie("session", "", time() - 3600);
-    header("Location: ./");
-    exit();
-}
-
-function EditProfile() {
-    $data = GetSiteData();
-    $user = GetUserData();
 
     if ($_POST["username"] != $user["username"]) {
-        if (strlen($_POST["username"]) < 4) {
-            Alert("Your username must be at least 4 characters long.");
-        }
-    
-        if (strlen($_POST["username"]) > 20) {
-            Alert("Your username must be less than 20 characters long.");
-        }
-    
-        if (preg_match("/^[a-zA-Z0-9_-]+$/", $_POST["username"]) == 0) {
-            Alert("Your username can only contain letters, numbers, underscores, and hyphens.");
-        }
+        $query = <<<SQL
+            SELECT * FROM `users` WHERE `username` = :username
+        SQL;
 
-        if (FindIndex($data["users"], "username", $_POST["username"]) != -1) {
+        $stmt = $db->prepare($query);
+        $stmt->bindValue(":username", $_POST["username"]);
+        $result = $stmt->execute()->fetchArray(SQLITE3_ASSOC);
+
+        if ($result != false) {
             Alert("That username is already taken.");
         }
 
-        $user["username"] = $_POST["username"];
+        $query = <<<SQL
+            UPDATE `users` SET `username` = :username WHERE `id` = :id    
+        SQL;
+
+        $stmt = $db->prepare($query);
+        $stmt->bindValue(":username", $_POST["username"]);
+        $stmt->bindValue(":id", $user["id"]);
+        $stmt->execute();
     }
 
     if ($_FILES["avatar"]["error"] == 0) {
@@ -205,40 +232,35 @@ function EditProfile() {
             Alert("Your avatar must be less than 2MB in size.");
         }
 
-        $extension = pathinfo($_FILES["avatar"]["name"], PATHINFO_EXTENSION);
-        $filename = $user["id"] . "." . $extension;
+        $extension = pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION);
+        $filename = uniqid("avatar-") . "." . $extension;
+        move_uploaded_file($_FILES["avatar"]["tmp_name"], "uploads/avatars/{$filename}");
 
-        if (move_uploaded_file($_FILES["avatar"]["tmp_name"], "uploads/avatars/{$filename}") == false) {
-            Alert("There was an error uploading your avatar.");
-        }
+        $query = <<<SQL
+            UPDATE `users` SET `avatar` = :avatar WHERE `id` = :id
+        SQL;
 
-        $user["avatar"] = $filename;
+        $stmt = $db->prepare($query);
+        $stmt->bindValue(":avatar", $filename);
+        $stmt->bindValue(":id", $user["id"]);
+        $stmt->execute();
     }
 
-    if (strlen($_POST["description"]) > 10000) {
-        Alert("Your description must be less than 10000 characters long.");
-    }
+    $query = <<<SQL
+        UPDATE `users` SET `description` = :description WHERE `id` = :id
+    SQL;
 
-    $user["description"] = $_POST["description"];
-    $userIndex = FindIndex($data["users"], "id", $user["id"]);
-
-    if ($userIndex == -1) {
-        Alert("There was an error updating your profile.");
-    }
-
-    $data["users"][$userIndex] = $user;
-
-    if (SetSiteData($data) == false) {
-        Alert("There was an error updating your profile.");
-    }
-
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(":description", $_POST["description"]);
+    $stmt->bindValue(":id", $user["id"]);
+    $stmt->execute();
     header("Location: user/?id={$user['username']}");
     exit();
 }
 
 function NewRequest() {
-    $data = GetSiteData();
-    $user = GetUserData();
+    $db = new SQLite3("database.db");
+    $user = GetUser();
 
     if (strlen($_POST["text"]) > 2000) {
         Alert("Your text must be less than 2000 characters long.");
@@ -258,64 +280,68 @@ function NewRequest() {
 
     $extension = pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION);
     $filename = uniqid("request") . "." . $extension;
+    move_uploaded_file($_FILES["image"]["tmp_name"], "uploads/requests/{$filename}");
 
-    if (move_uploaded_file($_FILES["image"]["tmp_name"], "uploads/requests/{$filename}") == false) {
-        Alert("There was an error uploading your image.");
-    }
+    $query = <<<SQL
+        INSERT INTO `requests` (`user_id`, `text`, `description`, `image`)
+        VALUES (:user_id, :text, :description, :image)
+    SQL;
 
-    $request = [
-        "id" => uniqid("request"),
-        "author" => $user["id"],
-        "text" => $_POST["text"],
-        "description" => $_POST["description"],
-        "image" => $filename,
-        "time" => time()
-    ];
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(":user_id", $user["id"]);
+    $stmt->bindValue(":text", $_POST["text"]);
+    $stmt->bindValue(":description", $_POST["description"]);
+    $stmt->bindValue(":image", $filename);
+    $stmt->execute();
 
-    $data["requests"][] = $request;
+    $query = <<<SQL
+        SELECT * FROM `requests` WHERE `id` = :id
+    SQL;
 
-    if (strlen(implode(", ", $_POST["tags"])) > 2000) {
-        Alert("Your tags must be less than 2000 characters long.");
-    }
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(":id", $db->lastInsertRowID());
+    $request = $stmt->execute()->fetchArray(SQLITE3_ASSOC);
 
     foreach ($_POST["tags"] as $tag) {
         if (substr($tag, 0, 1) == "-") {
             continue;
         }
 
-        if (preg_match("/^[a-zA-Z0-9_-]+$/", $tag) == 0) {
+        if (preg_match("/[^a-zA-Z0-9-_]/", $tag)) {
             continue;
         }
 
-        $tagIndex = FindIndex($data["tags"], "name", $tag);
+        $query = <<<SQL
+            SELECT * FROM `tags` WHERE `name` = :name
+        SQL;
 
-        if ($tagIndex == -1) {
-            $tagId = uniqid("tag");
+        $stmt = $db->prepare($query);
+        $stmt->bindValue(":name", $tag);
+        $result = $stmt->execute()->fetchArray(SQLITE3_ASSOC);
 
-            $newTag = [
-                "id" => $tagId,
-                "name" => $tag,
-                "time" => time()
-            ];
-
-            $data["tags"][] = $newTag;
+        if ($result) {
+            $id = $result["id"];
         } else {
-            $tagData = $data["tags"][$tagIndex];
-            $tagId = $tagData["id"];
+            $query = <<<SQL
+                INSERT INTO `tags` (`name`)
+                VALUES (:name)
+            SQL;
+
+            $stmt = $db->prepare($query);
+            $stmt->bindValue(":name", $tag);
+            $stmt->execute();
+            $id = $db->lastInsertRowID();
         }
 
-        $requestTag = [
-            "id" => uniqid("requestTag"),
-            "requestId" => $request["id"],
-            "tagId" => $tagId,
-            "time" => time()
-        ];
+        $query = <<<SQL
+            INSERT INTO `request_tags` (`request_id`, `tag_id`)
+            VALUES (:request_id, :tag_id)
+        SQL;
 
-        $data["requestTags"][] = $requestTag;
-    }
-
-    if (SetSiteData($data) == false) {
-        Alert("There was an error creating your request.");
+        $stmt = $db->prepare($query);
+        $stmt->bindValue(":request_id", $request["id"]);
+        $stmt->bindValue(":tag_id", $id);
+        $stmt->execute();
     }
 
     header("Location: request/?id={$request['id']}");
@@ -323,8 +349,11 @@ function NewRequest() {
 }
 
 function DefaultMethod() {
+    session_start();
+
     Breakpoint([
         "post" => $_POST,
-        "files" => $_FILES
+        "files" => $_FILES,
+        "session" => $_SESSION
     ]);
 }
