ffeaa1b5890464077ef06e54d4c85805afd80c96
ionvop <43801264+ionvop@users.noreply.github.com>
2025-08-01 15:37:33 +0800

    Create server.php

---
+++ server.php
@@ -0,0 +1,164 @@
+<?php
+
+include "common.php";
+
+if (isset($_POST["method"])) {
+    switch ($_POST["method"]) {
+        case "login":
+            login();
+            break;
+        case "verify":
+            verify();
+            break;
+        case "register":
+            register();
+            break;
+        default:
+            defaultMethod();
+            break;
+    }
+} else {
+    defaultMethod();
+}
+
+function login() {
+    global $BREVO_API_KEY;
+
+    if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) == false) {
+        alert("Invalid email.");
+    }
+
+    $code = substr(md5(time()), 0, 5);
+    session_start();
+    $_SESSION["code"] = $code;
+    $_SESSION["email"] = $_POST["email"];
+
+    $headers = [
+        "Content-Type: application/json",
+        "Accept: application/json",
+        "Api-Key: {$BREVO_API_KEY}"
+    ];
+
+    $body = [
+        "sender" => [
+            "name" => "SaucePls",
+            "email" => "ionvop@gmail.com"
+        ],
+        "to" => [
+            [
+                "email" => $_POST["email"]
+            ]
+        ],
+        "textContent" => "Your login code is: {$code}\n\nIf you did not request this code, you can safely ignore this email.",
+        "subject" => "SaucePls login code"
+    ];
+
+    // for testing
+    echo <<<HTML
+        <h1>{$body["subject"]}</h1>
+        <pre>{$body["textContent"]}</pre><br><br>
+        <a href="login/verify/">
+            <button>
+                Continue
+            </button>
+        </a>
+    HTML;
+
+    exit();
+
+    sendCurl("https://api.sendinblue.com/v3/smtp/email", "POST", $headers, json_encode($body));
+    header("Location: login/verify/");
+    exit();
+}
+
+function verify() {
+    $db = new SQLite3("database.db");
+    session_start();
+
+    if ($_POST["code"] != $_SESSION["code"]) {
+        Alert("Incorrect code.");
+    }
+
+    $query = <<<SQL
+        SELECT * FROM `users` WHERE `email` = :email
+    SQL;
+
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(":email", $_SESSION["email"]);
+    $user = $stmt->execute()->fetchArray(SQLITE3_ASSOC);
+
+    if ($user == false) {
+        header("Location: register/");
+        exit();
+    }
+
+    session_destroy();
+    $session = uniqid("session-");
+
+    $query = <<<SQL
+        UPDATE `users` SET `session` = :session WHERE `id` = :id
+    SQL;
+
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(":session", $session);
+    $stmt->bindValue(":id", $user["id"]);
+    $stmt->execute();
+    setcookie("session", $session, time() + (86400 * 30));
+    header("Location: ./");
+}
+
+function register() {
+    $db = new SQLite3("database.db");
+
+    if (strlen($_POST["username"]) < 4) {
+        alert("Your username must be at least 4 characters long.");
+    }
+
+    if (strlen($_POST["username"]) > 20) {
+        alert("Your username must be less than 20 characters long.");
+    }
+
+    if (preg_match("/[^a-zA-Z0-9-_]/", $_POST["username"])) {
+        alert("Your username can only contain letters, numbers, underscores, and hyphens.");
+    }
+
+    session_start();
+
+    $query = <<<SQL
+        SELECT * FROM `users` WHERE `username` = :username OR `email` = :email
+    SQL;
+
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(":username", $_POST["username"]);
+    $stmt->bindValue(":email", $_SESSION["email"]);
+    $user = $stmt->execute()->fetchArray(SQLITE3_ASSOC);
+
+    if ($user != false) {
+        alert("That username is already taken.");
+    }
+
+    $session = uniqid("session-");
+
+    $query = <<<SQL
+        INSERT INTO `users` (`username`, `email`, `session`) VALUES (:username, :email, :session)
+    SQL;
+
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(":username", $_POST["username"]);
+    $stmt->bindValue(":email", $_SESSION["email"]);
+    $stmt->bindValue(":session", $session);
+    $stmt->execute();
+    session_destroy();
+    setcookie("session", $session, time() + (86400 * 30));
+    header("Location: ./");
+}
+
+function defaultMethod() {
+    session_start();
+
+    Breakpoint([
+        "post" => $_POST,
+        "files" => $_FILES,
+        "session" => $_SESSION
+    ]);
+}
