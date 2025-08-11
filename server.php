2db9db021388783eee39a266ec8a8dba7acbcb45
ionvop <43801264+ionvop@users.noreply.github.com>
2025-08-11 10:29:28 +0800

    Update server.php

--- server.php
+++ server.php
@@ -19,6 +19,9 @@ if (isset($_POST["method"])) {
         case "upload":
             upload();
             break;
+        case "edit_profile":
+            editProfile();
+            break;
         default:
             defaultMethod();
             break;
@@ -211,6 +214,51 @@ function upload() {
     $stmt->bindValue(":text", $_POST["text"]);
     $stmt->execute();
     header("Location: post/?id={$db->lastInsertRowID()}");
+}
+
+function editProfile() {
+    $db = new SQLite3("database.db");
+    $user = getUser();
+
+    if ($user == false) {
+        alert("You must be logged in to edit your profile.");
+    }
+
+    if ($_FILES["avatar"]["error"] != 4) {
+        if ($_FILES["avatar"]["size"] > 4000000) {
+            alert("Your avatar was too large.\n\nMax size: 4MB.");
+        }
+
+        if ($_FILES["avatar"]["error"] != 0) {
+            alert("There was an error uploading your avatar.");
+        }
+
+        $filename = uniqid("avatar-") . "." . pathinfo($_FILES["avatar"]["name"], PATHINFO_EXTENSION);
+
+        if (move_uploaded_file($_FILES["avatar"]["tmp_name"], "uploads/avatars/{$filename}") == false) {
+            alert("There was an error uploading your avatar.");
+        }
+
+        $query = <<<SQL
+            UPDATE `users` SET `avatar` = :avatar WHERE `id` = :id
+        SQL;
+
+        $stmt = $db->prepare($query);
+        $stmt->bindValue(":avatar", $filename);
+        $stmt->bindValue(":id", $user["id"]);
+        $stmt->execute();
+    }
+
+    $query = <<<SQL
+        UPDATE `users` SET `username` = :username, `description` = :description WHERE `id` = :id
+    SQL;
+
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(":username", $_POST["username"]);
+    $stmt->bindValue(":description", $_POST["description"]);
+    $stmt->bindValue(":id", $user["id"]);
+    $stmt->execute();
+    header("Location: user/?id={$_POST["username"]}");
 }
 
 function defaultMethod() {
