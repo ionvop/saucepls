3ab2b2ca6467a6c856b3acbe19a8e9cd3b33438e
ionvop <ionvop@gmail.com>
2026-06-12 21:53:25 +0800

    Update index.php

--- login/verify/index.php
+++ login/verify/index.php
@@ -1,7 +1,7 @@
 <?php
 
 chdir("../../");
-include "common.php";
+require_once "common.php";
 
 ?>
 
@@ -15,62 +15,107 @@ include "common.php";
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <style>
-            body > .main {
-                display: grid;
-                grid-template-rows: max-content 1fr;
-                height: 100%;
 
-                & > .content {
-                    & > .verify {
-                        background-color: #222;
-                        border-radius: 1rem;
-
-                        & > .subtitle {
-                            padding-top: 0rem;
-                        }
-
-                        & > .input {
-                            & > input {
-                                width: 20rem;
-                            }
-                        }
-                    }
-                }
-            }
         </style>
     </head>
     <body>
-        <div class="main">
-            <?=renderHeader()?>
-            <div class="content -center__flex">
-                <form action="server.php" class="-form verify -pad" method="post" enctype="multipart/form-data">
-                    <div class="title -title -pad -center">
-                        Enter your login code
-                    </div>
-                    <div class="subtitle -subtitle -pad -center">
-                        Don't forget to check your spam folder.
-                    </div>
-                    <div class="input -pad -center">
-                        <input class="-input" name="code" placeholder="Code">
-                    </div>
-                    <div class="send -pad -center">
-                        <button class="-button" name="method" value="verify">
-                            <div class="-iconlabel">
-                                <div class="icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="m382-354 339-339q12-12 28-12t28 12q12 12 12 28.5T777-636L410-268q-12 12-28 12t-28-12L182-440q-12-12-11.5-28.5T183-497q12-12 28.5-12t28.5 12l142 143Z"/></svg>
-                                </div>
-                                <div class="label">
+        <div style="
+            display: grid;
+            grid-template-columns: max-content 1fr;
+            height: 100%;
+            box-sizing: border-box;">
+            <?= renderNavigation() ?>
+            <div style="
+                display: grid;
+                grid-template-rows: max-content 1fr;">
+                <?= renderHeader() ?>
+                <div style="
+                    display: grid;
+                    grid-template-columns: 1fr max-content;">
+                    <div style="
+                        display: grid;
+                        grid-template-columns: 1fr max-content 1fr;">
+                        <div></div>
+                        <div style="
+                            padding: 5rem;">
+                            <form style="
+                                padding: 1rem;
+                                background-color: #222;
+                                border-radius: 1rem;
+                                width: 20rem;"
+                                action="server.php"
+                                method="post"
+                                enctype="multipart/form-data">
+                                <div style="
+                                    padding: 1rem;
+                                    text-align: center;
+                                    font-size: 1.5rem;
+                                    font-weight: bold;">
                                     Verify
                                 </div>
-                            </div>
-                        </button>
+                                <div style="
+                                    padding: 1rem;
+                                    font-size: 0.7rem;
+                                    text-align: center;
+                                    color: #aaa;">
+                                    Enter the code that was sent to your email.
+                                </div>
+                                <div style="
+                                    padding: 1rem;">
+                                    Enter your code:
+                                </div>
+                                <div style="
+                                    padding: 1rem;
+                                    padding-top: 0rem;">
+                                    <input name="code"
+                                        placeholder="Code"
+                                        required>
+                                </div>
+                                <div style="
+                                    padding: 1rem;
+                                    text-align: center;">
+                                    <button name="method" value="verify">
+                                        <div style="
+                                            display: grid;
+                                            grid-template-columns: max-content 1fr;">
+                                            <div style="
+                                                display: flex;
+                                                align-items: center;">
+                                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m382-354 339-339q12-12 28-12t28 12q12 12 12 28.5T777-636L410-268q-12 12-28 12t-28-12L182-440q-12-12-11.5-28.5T183-497q12-12 28.5-12t28.5 12l142 143Z"/></svg>
+                                            </div>
+                                            <div style="
+                                                display: flex;
+                                                align-items: center;
+                                                padding-left: 0.5rem;">
+                                                Submit
+                                            </div>
+                                        </div>
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                        <div></div>
                     </div>
-                </form>
+                    <div style="
+                        background-color: #222;
+                        border-left: 1px solid #555;
+                        width: 15rem;">
+                        <div style="
+                            padding: 1rem;
+                            font-size: 1.5rem;
+                            text-align: center;">
+                            More Sign-In Options
+                        </div>
+                        <div style="
+                            padding: 1rem;
+                            font-size: 0.7rem;
+                            color: #aaa;
+                            text-align: center;">
+                            This feature is still in development
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <script src="script.js"></script>
-        <script>
-
-        </script>
     </body>
 </html>
