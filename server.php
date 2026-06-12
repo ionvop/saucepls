494a91bc58ab0ad11c52aae07139f4a2a5318bfd
ionvop <ionvop@gmail.com>
2026-06-12 21:53:32 +0800

    Update server.php

--- server.php
+++ server.php
@@ -1,6 +1,6 @@
 <?php
 
-include "common.php";
+require_once "common.php";
 
 if (isset($_POST["method"])) {
     switch ($_POST["method"]) {
@@ -15,18 +15,6 @@ if (isset($_POST["method"])) {
             break;
         case "logout":
             logout();
-            break;
-        case "upload":
-            upload();
-            break;
-        case "edit_profile":
-            editProfile();
-            break;
-        case "user_comment":
-            userComment();
-            break;
-        case "delete_user_comment":
-            deleteUserComment();
             break;
         default:
             defaultMethod();
@@ -43,47 +31,43 @@ function login() {
         alert("Invalid email.");
     }
 
-    $code = substr(md5(time()), 0, 5);
+    $code = "";
+
+    for ($i = 0; $i < 6; $i++) {
+        $code .= rand(0, 9);
+    }
+
     session_start();
     $_SESSION["code"] = $code;
     $_SESSION["email"] = $_POST["email"];
 
-    $headers = [
-        "Content-Type: application/json",
-        "Accept: application/json",
-        "Api-Key: {$BREVO_API_KEY}"
-    ];
-
-    $body = [
-        "sender" => [
-            "name" => "SaucePls",
-            "email" => "ionvop@gmail.com"
+    $response = fetch("https://api.sendinblue.com/v3/smtp/email", [
+        "method" => "POST",
+        "headers" => [
+            "Content-Type" => "application/json",
+            "Accept" => "application/json",
+            "Api-Key" => $BREVO_API_KEY
         ],
-        "to" => [
-            [
-                "email" => $_POST["email"]
-            ]
-        ],
-        "textContent" => "Your login code is: {$code}\n\nIf you did not request this code, you can safely ignore this email.",
-        "subject" => "SaucePls login code"
-    ];
+        "body" => json_encode([
+            "sender" => [
+                "name" => "SaucePls",
+                "email" => "ionvop@gmail.com"
+            ],
+            "to" => [
+                [
+                    "email" => $_POST["email"]
+                ]
+            ],
+            "textContent" => "Your login code is: {$code}\n\nIf you did not request this code, you can safely ignore this email.",
+            "subject" => "SaucePls login code"
+        ])
+    ]);
 
-    // for testing
-    echo <<<HTML
-        <h1>{$body["subject"]}</h1>
-        <pre>{$body["textContent"]}</pre><br><br>
-        <a href="login/verify/">
-            <button>
-                Continue
-            </button>
-        </a>
-    HTML;
+    if ($response["ok"] == false) {
+        alert("Failed to send email.");
+    }
 
-    exit();
-
-    sendCurl("https://api.sendinblue.com/v3/smtp/email", "POST", $headers, json_encode($body));
     header("Location: login/verify/");
-    exit();
 }
 
 function verify() {
@@ -91,7 +75,7 @@ function verify() {
     session_start();
 
     if ($_POST["code"] != $_SESSION["code"]) {
-        Alert("Incorrect code.");
+        alert("Invalid code.");
     }
 
     $query = <<<SQL
@@ -104,7 +88,7 @@ function verify() {
 
     if ($user == false) {
         header("Location: register/");
-        exit();
+        exit;
     }
 
     session_destroy();
@@ -181,156 +165,13 @@ function logout() {
     $stmt->execute();
     setcookie("session", "", time() - 3600);
     header("Location: ./");
-    exit();
-}
-
-function upload() {
-    $db = new SQLite3("database.db");
-    $user = getUser();
-
-    if ($user == false) {
-        alert("You must be logged in to upload files.");
-    }
-
-    if ($_FILES["image"]["error"] != 0) {
-        alert("There was an error uploading your image.");
-    }
-
-    if ($_FILES["image"]["size"] > 4000000) {
-        alert("Your image was too large.\n\nMax size: 4MB.");
-    }
-
-    $filename = uniqid("post-") . "." . pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION);
-    
-    if (move_uploaded_file($_FILES["image"]["tmp_name"], "uploads/posts/{$filename}") == false) {
-        alert("There was an error uploading your image.");
-    }
-
-    $query = <<<SQL
-        INSERT INTO `posts` (`user_id`, `image`, `title`, `description`, `tags`, `text`)
-        VALUES (:user_id, :image, :title, :description, :tags, :text)
-    SQL;
-
-    $stmt = $db->prepare($query);
-    $stmt->bindValue(":user_id", $user["id"]);
-    $stmt->bindValue(":image", $filename);
-    $stmt->bindValue(":title", $_POST["title"]);
-    $stmt->bindValue(":description", $_POST["description"]);
-    $stmt->bindValue(":tags", $_POST["tags"]);
-    $stmt->bindValue(":text", $_POST["text"]);
-    $stmt->execute();
-    header("Location: post/?id={$db->lastInsertRowID()}");
-}
-
-function editProfile() {
-    $db = new SQLite3("database.db");
-    $user = getUser();
-
-    if ($user == false) {
-        alert("You must be logged in to edit your profile.");
-    }
-
-    if ($_FILES["avatar"]["error"] != 4) {
-        if ($_FILES["avatar"]["size"] > 4000000) {
-            alert("Your avatar was too large.\n\nMax size: 4MB.");
-        }
-
-        if ($_FILES["avatar"]["error"] != 0) {
-            alert("There was an error uploading your avatar.");
-        }
-
-        $filename = uniqid("avatar-") . "." . pathinfo($_FILES["avatar"]["name"], PATHINFO_EXTENSION);
-
-        if (move_uploaded_file($_FILES["avatar"]["tmp_name"], "uploads/avatars/{$filename}") == false) {
-            alert("There was an error uploading your avatar.");
-        }
-
-        $query = <<<SQL
-            UPDATE `users` SET `avatar` = :avatar WHERE `id` = :id
-        SQL;
-
-        $stmt = $db->prepare($query);
-        $stmt->bindValue(":avatar", $filename);
-        $stmt->bindValue(":id", $user["id"]);
-        $stmt->execute();
-    }
-
-    if ($_POST["username"] != $user["username"]) {
-        if (getUserByName($_POST["username"]) != false) {
-            alert("That username is already taken.");
-        }
-    }
-
-    $query = <<<SQL
-        UPDATE `users` SET `username` = :username, `description` = :description WHERE `id` = :id
-    SQL;
-
-    $stmt = $db->prepare($query);
-    $stmt->bindValue(":username", $_POST["username"]);
-    $stmt->bindValue(":description", $_POST["description"]);
-    $stmt->bindValue(":id", $user["id"]);
-    $stmt->execute();
-    header("Location: user/?id={$_POST["username"]}");
-}
-
-function userComment() {
-    $db = new SQLite3("database.db");
-    $user = getUser();
-    $target = getUserByName($_POST["target_name"]);
-
-    if ($user == false) {
-        alert("You must be logged in to comment.");
-    }
-
-    $query = <<<SQL
-        INSERT INTO `user_comments` (`user_id`, `target_id`, `content`)
-        VALUES (:user_id, :target_id, :content)
-    SQL;
-
-    $stmt = $db->prepare($query);
-    $stmt->bindValue(":user_id", $user["id"]);
-    $stmt->bindValue(":target_id", $target["id"]);
-    $stmt->bindValue(":content", $_POST["comment"]);
-    $stmt->execute();
-    header("Location: user/?id={$_POST["target_name"]}");
-}
-
-function deleteUserComment() {
-    $db = new SQLite3("database.db");
-    $user = getUser();
-
-    if ($user == false) {
-        alert("You must be logged in to delete a comment.");
-    }
-
-    $query = <<<SQL
-        SELECT * FROM `user_comments` WHERE `id` = :id
-    SQL;
-
-    $stmt = $db->prepare($query);
-    $stmt->bindValue(":id", $_POST["id"]);
-    $comment = $stmt->execute()->fetchArray(SQLITE3_ASSOC);
-
-    if ($comment["user_id"] != $user["id"] && $comment["target_id"] != $user["id"] && $user["type"] != "moderator" && $user["type"] != "admin") {
-        alert("You cannot delete this comment.");
-    }
-
-    $target = getUserById($comment["target_id"]);
-
-    $query = <<<SQL
-        DELETE FROM `user_comments` WHERE `id` = :id
-    SQL;
-
-    $stmt = $db->prepare($query);
-    $stmt->bindValue(":id", $_POST["id"]);
-    $stmt->execute();
-    header("Location: user/?id={$target["username"]}");
+    exit;
 }
 
 function defaultMethod() {
     session_start();
 
-    Breakpoint([
+    breakpoint([
         "post" => $_POST,
         "files" => $_FILES,
         "session" => $_SESSION
