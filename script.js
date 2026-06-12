ee4d90724c264983e2463b430d75f7e39a0658c3
ionvop <ionvop@gmail.com>
2026-06-12 23:42:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,10 @@
     for (let element of document.querySelectorAll("*")) {
         element.style.setProperty("--test", "test");
         element.style.removeProperty("--test");
+
+        // check if element has data-timestamp attribute
+        if (element.hasAttribute("data-timestamp")) {
+            element.title = new Date(element.getAttribute("data-timestamp") * 1000).toLocaleString();
+        }
     }
 })();
