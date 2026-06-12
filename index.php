93f650770bb2ff9744468cb0383ce7aa07527f25
ionvop <ionvop@gmail.com>
2026-06-12 23:42:40 +0800

    Update index.php

--- index.php
+++ index.php
@@ -78,29 +78,25 @@ require_once "common.php";
                                 placeholder="Keywords or tags...">
                         </div>
                         <div style="
-                            display: grid;
-                            grid-template-columns: 1fr max-content;
-                            padding: 1rem;">
-                            <div></div>
-                            <div>
-                                <button>
+                            padding: 1rem;
+                            text-align: center;">
+                            <button>
+                                <div style="
+                                    display: grid;
+                                    grid-template-columns: max-content 1fr;">
                                     <div style="
-                                        display: grid;
-                                        grid-template-columns: max-content 1fr;">
-                                        <div style="
-                                            display: flex;
-                                            align-items: center;">
-                                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M380-320q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l224 224q11 11 11 28t-11 28q-11 11-28 11t-28-11L532-372q-30 24-69 38t-83 14Zm0-80q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
-                                        </div>
-                                        <div style="
-                                            display: flex;
-                                            align-items: center;
-                                            padding-left: 0.5rem;">
-                                            Search
-                                        </div>
+                                        display: flex;
+                                        align-items: center;">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M380-320q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l224 224q11 11 11 28t-11 28q-11 11-28 11t-28-11L532-372q-30 24-69 38t-83 14Zm0-80q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
                                     </div>
-                                </button>
-                            </div>
+                                    <div style="
+                                        display: flex;
+                                        align-items: center;
+                                        padding-left: 0.5rem;">
+                                        Search
+                                    </div>
+                                </div>
+                            </button>
                         </div>
                     </form>
                 </div>
