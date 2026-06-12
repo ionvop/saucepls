81060cc54d4f6e98ba9a65a1537f6094d81445b8
ionvop <ionvop@gmail.com>
2026-06-12 21:53:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,127 +1,114 @@
 <?php
 
-include "common.php";
-$db = new SQLite3("database.db");
+require_once "common.php";
 
 ?>
 
 <html>
     <head>
         <title>
-            SaucePls
+            Home | SaucePls
         </title>
         <base href="./">
         <link rel="stylesheet" href="style.css">
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <style>
-            body > .main {
-                & > .page {
-                    & > .content {
-                        & > .posts {
-                            display: grid;
-                            grid-template-columns: repeat(2, 1fr);
-                        }
-                    }
-                }
-            }
+
         </style>
     </head>
     <body>
-        <div class="main -main">
-            <?=renderNavigation()?>
-            <div class="page">
-                <?=renderHeader()?>
-                <div class="content -pad">
-                    <div class="welcome -pad -title -center">
-                        Welcome to SaucePls
+        <div style="
+            display: grid;
+            grid-template-columns: max-content 1fr;
+            height: 100%;
+            box-sizing: border-box;">
+            <?= renderNavigation() ?>
+            <div style="
+                display: grid;
+                grid-template-rows: max-content 1fr;">
+                <?= renderHeader() ?>
+                <div style="
+                    display: grid;
+                    grid-template-columns: 1fr max-content;">
+                    <div>
+
                     </div>
-                    <div class="subtitle -pad -center">
-                        Upload images, anime screenshots, or manga panels and let the community help you find the source or artist!
-                    </div>
-                    <div class="posts">
-                        <div class="trending -posts">
-                            <div class="item">
-                                <div class="image">
-                                    <img src="assets/image.png">
-                                </div>
-                                <div class="content">
-                                    <div class="title">
-                                        <div class="text">
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores quia perferendis aliquid quae eveniet. Pariatur voluptatibus necessitatibus velit illum aperiam perferendis facere enim aut, quisquam dolorum veritatis a odit minus.
-                                        </div>
-                                    </div>
-                                    <div class="tags">
-                                        <div class="tag">
-                                            <div class="box -subtitle">
-                                                1girl
-                                            </div>
-                                        </div>
-                                        <div class="tag">
-                                            <div class="box -subtitle">
-                                                blue_eyes
-                                            </div>
-                                        </div>
-                                        <div class="tag">
-                                            <div class="box -subtitle">
-                                                blonde_hair
-                                            </div>
-                                        </div>
-                                        <div class="tag">
-                                            <div class="box -subtitle">
-                                                1girl
-                                            </div>
-                                        </div>
-                                        <div class="tag">
-                                            <div class="box -subtitle">
-                                                blue_eyes
-                                            </div>
-                                        </div>
-                                        <div class="tag">
-                                            <div class="box -subtitle">
-                                                blonde_hair
-                                            </div>
-                                        </div>
-                                        <div class="tag">
-                                            <div class="box -subtitle">
-                                                1girl
-                                            </div>
-                                        </div>
-                                        <div class="tag">
-                                            <div class="box -subtitle">
-                                                blue_eyes
-                                            </div>
-                                        </div>
-                                        <div class="tag">
-                                            <div class="box -subtitle">
-                                                blonde_hair
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="description">
-                                        <div class="text">
-                                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur ad, aperiam corporis a, modi explicabo perspiciatis, illum neque esse nisi delectus error! Commodi consectetur reiciendis autem, et sed totam eum?
-                                        </div>
-                                    </div>
-                                    <div class="details -subtitle">
-                                        1 day ago
-                                    </div>
-                                </div>
-                                <div class="status -center__flex">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="m382-354 339-339q12-12 28-12t28 12q12 12 12 28.5T777-636L410-268q-12 12-28 12t-28-12L182-440q-12-12-11.5-28.5T183-497q12-12 28.5-12t28.5 12l142 143Z"/></svg>
-                                </div>
+                    <form style="
+                        background-color: #222;
+                        border-left: 1px solid #555;
+                        width: 15rem;">
+                        <div style="
+                            padding: 1rem;
+                            text-align: center;
+                            font-size: 1.5rem;">
+                            Search
+                        </div>
+                        <div style="
+                            display: grid;
+                            grid-template-columns: max-content 1fr;">
+                            <div style="
+                                display: flex;
+                                align-items: center;
+                                padding: 1rem;">
+                                Sort:
+                            </div>
+                            <div style="
+                                display: flex;
+                                align-items: center;
+                                padding: 1rem;
+                                padding-left: 0rem;">
+                                <select name="sort">
+                                    <option value="recent">
+                                        Recent
+                                    </option>
+                                    <option value="trending">
+                                        Trending
+                                    </option>
+                                    <option value="follow">
+                                        Popular
+                                    </option>
+                                </select>
                             </div>
                         </div>
-                        <div class="recent">
-
+                        <div style="
+                            padding: 1rem;">
+                            <input type="text"
+                                name="q"
+                                placeholder="Keywords or tags...">
                         </div>
-                    </div>
+                        <div style="
+                            display: grid;
+                            grid-template-columns: 1fr max-content;
+                            padding: 1rem;">
+                            <div></div>
+                            <div>
+                                <button>
+                                    <div style="
+                                        display: grid;
+                                        grid-template-columns: max-content 1fr;">
+                                        <div style="
+                                            display: flex;
+                                            align-items: center;">
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M380-320q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l224 224q11 11 11 28t-11 28q-11 11-28 11t-28-11L532-372q-30 24-69 38t-83 14Zm0-80q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
+                                        </div>
+                                        <div style="
+                                            display: flex;
+                                            align-items: center;
+                                            padding-left: 0.5rem;">
+                                            Search
+                                        </div>
+                                    </div>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-        <script src="script.js"></script>
-        <script>
-
-        </script>
     </body>
+    <script src="script.js"></script>
+    <script>
+
+    </script>
 </html>
