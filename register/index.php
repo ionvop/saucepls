aab3709d66e7d1646720429ea3f9e86e9bb0f906
ionvop <ionvop@gmail.com>
2026-06-12 21:53:28 +0800

    Update index.php

--- register/index.php
+++ register/index.php
@@ -1,7 +1,7 @@
 <?php
 
 chdir("../");
-include "common.php";
+require_once "common.php";
 
 ?>
 
@@ -15,60 +15,107 @@ include "common.php";
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <style>
-            body > .main {
-                display: grid;
-                grid-template-rows: max-content 1fr;
-                height: 100%;
-                
-                & > .content {
-                    & > .register {
-                        background-color: #222;
-                        border-radius: 1rem;
 
-                        & > .subtitle {
-                            padding-top: 0rem;
-                        }
-
-                        & > .input {
-                            & > input {
-                                width: 20rem;
-                            }
-                        }
-                    }
-                }
-            }
         </style>
     </head>
     <body>
-        <div class="main">
-            <?=renderHeader()?>
-            <div class="content -center__flex">
-                <form action="server.php" class="-form register -pad" method="post" enctype="multipart/form-data">
-                    <div class="title -title -pad -center">
-                        Enter your username
-                    </div>
-                    <div class="subtitle -subtitle -pad -center">
-                        Your email hasn't been registered yet.<br><br>
-                        IMPORTANT:<br>
-                        The account will be permanently tied to your email and cannot be changed.<br>
-                        If you lose access to your email, you might not be able to recover it.
-                    </div>
-                    <div class="input -pad -center">
-                        <input class="-input" name="username" placeholder="Username">
-                    </div>
-                    <div class="send -pad -center">
-                        <button class="-button" name="method" value="register">
-                            <div class="-iconlabel">
-                                <div class="icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M400-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160q-33 0-56.5-23.5T80-240v-32q0-33 17-62t47-44q51-26 115-44t141-18h26.5q12.5 0 25.5 2 20 2 26 21t-8 33l-17 17q-31 31-35 73t19 77q12 19 3.5 38T412-160H160Zm462-96 174-174q11-11 28-11t28 11q11 11 11 28t-11 28L650-172q-12 12-28 12t-28-12l-82-82q-11-11-11-28t11-28q11-11 28-11t28 11l54 54Z"/></svg>
-                                </div>
-                                <div class="label">
+        <div style="
+            display: grid;
+            grid-template-columns: max-content 1fr;
+            height: 100%;
+            box-sizing: border-box;">
+            <?= renderNavigation() ?>
+            <div style="
+                display: grid;
+                grid-template-rows: max-content 1fr;">
+                <?= renderHeader() ?>
+                <div style="
+                    display: grid;
+                    grid-template-columns: 1fr max-content;">
+                    <div style="
+                        display: grid;
+                        grid-template-columns: 1fr max-content 1fr;">
+                        <div></div>
+                        <div style="
+                            padding: 5rem;">
+                            <form style="
+                                padding: 1rem;
+                                background-color: #222;
+                                border-radius: 1rem;
+                                width: 20rem;"
+                                action="server.php"
+                                method="post"
+                                enctype="multipart/form-data">
+                                <div style="
+                                    padding: 1rem;
+                                    text-align: center;
+                                    font-size: 1.5rem;
+                                    font-weight: bold;">
                                     Register
                                 </div>
-                            </div>
-                        </button>
+                                <div style="
+                                    padding: 1rem;
+                                    font-size: 0.7rem;
+                                    text-align: center;
+                                    color: #aaa;">
+                                    Your email is not registered yet. Pick a username for your new account.
+                                </div>
+                                <div style="
+                                    padding: 1rem;">
+                                    Enter your username:
+                                </div>
+                                <div style="
+                                    padding: 1rem;
+                                    padding-top: 0rem;">
+                                    <input name="username"
+                                        placeholder="Username"
+                                        maxlength="20"
+                                        required>
+                                </div>
+                                <div style="
+                                    padding: 1rem;
+                                    text-align: center;">
+                                    <button name="method" value="register">
+                                        <div style="
+                                            display: grid;
+                                            grid-template-columns: max-content 1fr;">
+                                            <div style="
+                                                display: flex;
+                                                align-items: center;">
+                                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m382-354 339-339q12-12 28-12t28 12q12 12 12 28.5T777-636L410-268q-12 12-28 12t-28-12L182-440q-12-12-11.5-28.5T183-497q12-12 28.5-12t28.5 12l142 143Z"/></svg>
+                                            </div>
+                                            <div style="
+                                                display: flex;
+                                                align-items: center;
+                                                padding-left: 0.5rem;">
+                                                Submit
+                                            </div>
+                                        </div>
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                        <div></div>
                     </div>
-                </form>
+                    <div style="
+                        background-color: #222;
+                        border-left: 1px solid #555;
+                        width: 15rem;">
+                        <div style="
+                            padding: 1rem;
+                            font-size: 1.5rem;
+                            text-align: center;">
+                            More Sign-In Options
+                        </div>
+                        <div style="
+                            padding: 1rem;
+                            font-size: 0.7rem;
+                            color: #aaa;
+                            text-align: center;">
+                            This feature is still in development
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <script src="script.js"></script>
