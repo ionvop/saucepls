f643d62621de2e1421a0b9699697a976105d2c65
ionvop <ionvop@gmail.com>
2026-06-13 14:21:43 +0800

    Update server.php

--- server.php
+++ server.php
@@ -16,6 +16,9 @@ if (isset($_POST["method"])) {
         case "logout":
             logout();
             break;
+        case "edit_profile":
+            editProfile();
+            break;
         default:
             defaultMethod();
             break;
@@ -166,6 +169,57 @@ function logout() {
     setcookie("session", "", time() - 3600);
     header("Location: ./");
     exit;
+}
+
+function editProfile() {
+    $db = new SQLite3("database.db");
+    $user = getUser();
+
+    if ($user == false) {
+        alert("You must be logged in to edit your profile.");
+    }
+
+    if ($_FILES["avatar"]["error"] != 4) {
+        if ($_FILES["avatar"]["size"] > 4000000) {
+            alert("Your avatar was too large.\n\nMax size: 4MB.");
+        }
+
+        if ($_FILES["avatar"]["error"] != 0) {
+            alert("There was an error uploading your avatar.");
+        }
+
+        $filename = uniqid("avatar-") . "." . pathinfo($_FILES["avatar"]["name"], PATHINFO_EXTENSION);
+
+        if (move_uploaded_file($_FILES["avatar"]["tmp_name"], "uploads/avatars/{$filename}") == false) {
+            alert("There was an error uploading your avatar.");
+        }
+
+        $query = <<<SQL
+            UPDATE `users` SET `avatar` = :avatar WHERE `id` = :id
+        SQL;
+
+        $stmt = $db->prepare($query);
+        $stmt->bindValue(":avatar", $filename);
+        $stmt->bindValue(":id", $user["id"]);
+        $stmt->execute();
+    }
+
+    if ($_POST["username"] != $user["username"]) {
+        if (getUserByName($_POST["username"]) != false) {
+            alert("That username is already taken.");
+        }
+    }
+
+    $query = <<<SQL
+        UPDATE `users` SET `username` = :username, `description` = :description WHERE `id` = :id
+    SQL;
+
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(":username", $_POST["username"]);
+    $stmt->bindValue(":description", $_POST["description"]);
+    $stmt->bindValue(":id", $user["id"]);
+    $stmt->execute();
+    header("Location: user/?id={$_POST["username"]}");
 }
 
 function defaultMethod() {
