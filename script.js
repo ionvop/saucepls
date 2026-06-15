faad5132ba38c8b745fafc263acbd61e2910f787
ionvop <ionvop@gmail.com>
2026-06-16 01:07:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const g_inputTags = document.getElementById("g_inputTags");
+const g_panelSuggestions = document.getElementById("g_panelSuggestions");
+let g_timeout;
+
 (() => {
     for (let element of document.querySelectorAll("*")) {
         element.style.setProperty("--test", "test");
@@ -9,3 +13,87 @@
         }
     }
 })();
+
+g_inputTags.oninput = async () => {
+    clearTimeout(g_timeout);
+    g_timeout = setTimeout(g_loadSuggestions, 1000);
+}
+
+async function g_loadSuggestions() {
+    const currentTag = g_inputTags.value.split(/\s+/).pop();
+
+    if (!currentTag) {
+        g_panelSuggestions.style.display = "none";
+        return;
+    }
+
+    const response = await fetch("server.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            method: "api",
+            action: "suggest_tags",
+            tag: currentTag
+        })
+    });
+
+    const tags = await response.json();
+    g_renderSuggestions(tags);
+}
+
+function g_renderSuggestions(tags) {
+    g_panelSuggestions.innerHTML = "";
+
+    if (!tags.length) {
+        g_panelSuggestions.style.display = "none";
+        return;
+    }
+
+    tags.forEach(tag => {
+        const div = document.createElement("div");
+
+        div.innerHTML = /*html*/`
+            <div style="
+                display: grid;
+                grid-template-columns: 1fr max-content;"
+                class="-tab"
+                data-tag="${tag.name}"
+                onclick="g_insertTag(this)">
+                <div style="
+                    padding: 1rem;">
+                    ${tag.name}
+                </div>
+                <div style="
+                    display: flex;
+                    align-items: center;
+                    padding: 1rem;
+                    padding-left: 0rem;
+                    font-size: 0.7rem;
+                    color: #aaa;">
+                    ${tag.count}
+                </div>
+            </div>
+        `;
+
+        g_panelSuggestions.appendChild(div.firstElementChild);
+    });
+
+    g_panelSuggestions.style.display = "block";
+}
+
+function g_insertTag(element) {
+    const tagName = element.dataset.tag;
+    const parts = g_inputTags.value.trim().split(/\s+/);
+
+    if (parts.length == 0) {
+        g_inputTags.value = tagName + " ";
+    } else {
+        parts[parts.length - 1] = tagName;
+        g_inputTags.value = parts.join(" ") + " ";
+    }
+
+    g_panelSuggestions.style.display = "none";
+    g_inputTags.focus();
+}
