eab226e15896953fb78a2e9e717b8dbc903535ab
ionvop <ionvop@gmail.com>
2026-08-06 00:02:17 +0800

    Create create.blade.php

---
+++ resources/views/pages/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'New Request - ' . config('app.name', 'SaucePls'))
+
+@section('content')
+    {{-- New sauce request form will be rendered here. --}}
+@endsection
