ce2e7865eab0481df12bab560ba2524feb2ff221
ionvop <ionvop@gmail.com>
2026-08-06 00:02:33 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.home');
+})->name('home');
+
+Route::get('/search', function () {
+    return view('pages.search');
+})->name('search');
+
+Route::get('/notifications', function () {
+    return view('pages.notifications');
+})->name('notifications');
+
+Route::get('/profile', function () {
+    return view('pages.profile');
+})->name('profile');
+
+Route::get('/settings', function () {
+    return view('pages.settings');
+})->name('settings');
+
+Route::get('/create', function () {
+    return view('pages.create');
+})->name('create');
