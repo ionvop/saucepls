5da445009e03528220b2493b18e24499c2a6d77a
ionvop <ionvop@gmail.com>
2026-08-06 00:02:22 +0800

    Create notifications.blade.php

---
+++ resources/views/pages/notifications.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Notifications - ' . config('app.name', 'SaucePls'))
+
+@section('content')
+    {{-- Notifications page will be rendered here. --}}
+@endsection
