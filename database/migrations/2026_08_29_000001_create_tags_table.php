fbae791ba419ee2890ff117b89b173089f0942f0
ionvop <ionvop@gmail.com>
2026-08-29 21:21:18 +0800

    Create 2026_08_29_000001_create_tags_table.php

---
+++ database/migrations/2026_08_29_000001_create_tags_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->text('description')->default('A wiki has not been written for this tag yet.');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tags');
+    }
+};
