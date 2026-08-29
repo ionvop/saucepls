810c9d86bcb0b44890c9f8d8b82b9219007e6d5e
ionvop <ionvop@gmail.com>
2026-08-29 20:21:25 +0800

    Delete create.blade.php

--- resources/views/pages/create.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'New Request - ' . config('app.name', 'SaucePls'))
-
-@section('content')
-    {{-- New sauce request form will be rendered here. --}}
-@endsection
