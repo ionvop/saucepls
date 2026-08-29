8051de11b894ac918d5f74d90b16697493dc7f30
ionvop <ionvop@gmail.com>
2026-08-29 21:21:04 +0800

    Update UpdateSauceRequestRequest.php

--- app/Http/Requests/UpdateSauceRequestRequest.php
+++ app/Http/Requests/UpdateSauceRequestRequest.php
@@ -21,6 +21,7 @@ class UpdateSauceRequestRequest extends FormRequest
             'title' => ['nullable', 'string', 'max:120'],
             'description' => ['nullable', 'string', 'max:5000'],
             'is_explicit' => ['sometimes', 'boolean'],
+            'tags' => ['nullable', 'string', 'max:1000'],
         ];
     }
 }
