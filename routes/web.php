8aef1ffe84d1b442e58f5a0c385bcdb4fb8d7c9a
ionvop <ionvop@gmail.com>
2026-09-06 20:15:32 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -164,6 +164,7 @@ Route::middleware('auth')->group(function () {
 Route::get('/profile', [ProfileController::class, 'show'])->name('profile');
 Route::get('/u/{user}/followers', [ProfileController::class, 'followers'])->name('profile.followers');
 Route::get('/u/{user}/following', [ProfileController::class, 'following'])->name('profile.following');
+Route::get('/u/{user}/accepted-answers', [ProfileController::class, 'acceptedAnswers'])->name('profile.accepted-answers');
 Route::get('/u/{username}', [ProfileController::class, 'show'])->name('profile.show');
 
 // --- Settings (guest-accessible; authed users save via the auth-only PUT route) ---
