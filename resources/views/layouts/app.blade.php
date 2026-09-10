0daaccd60cda02cdcca4d2aa8bd4512ab68f2ced
ionvop <ionvop@gmail.com>
2026-09-10 21:48:33 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -272,6 +272,10 @@
                 @endauth
 
                 @guest
+                    <a href="{{ route('settings') }}" class="flex flex-col items-center gap-0.5 rounded-lg px-3 py-2 text-xs text-gray-400 transition hover:text-white {{ request()->routeIs('settings') ? 'text-[#5555AA]!' : '' }}">
+                        <x-lucide-settings class="h-5 w-5" />
+                        <span>Settings</span>
+                    </a>
                     <a href="{{ route('login') }}" class="flex flex-col items-center gap-0.5 rounded-lg px-3 py-2 text-xs text-gray-400 transition hover:text-white {{ request()->routeIs('login') ? 'text-[#5555AA]!' : '' }}">
                         <x-lucide-log-in class="h-5 w-5" />
                         <span>Login</span>
