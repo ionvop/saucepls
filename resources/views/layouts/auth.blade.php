d63b2c052ad83860327acd1ac9d2aafcae950abb
ionvop <ionvop@gmail.com>
2026-09-11 03:44:00 +0800

    Update auth.blade.php

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -7,6 +7,14 @@
 
         <title>@yield('title', config('app.name', 'SaucePls'))</title>
 
+        <!-- Icons / PWA -->
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="manifest" href="/manifest.webmanifest">
+        <meta name="theme-color" content="#111111">
+
         @fonts
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
