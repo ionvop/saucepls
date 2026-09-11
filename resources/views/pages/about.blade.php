97e84a2974c5ed8093cfe81a42e0990cd1818c3e
ionvop <ionvop@gmail.com>
2026-09-11 20:03:01 +0800

    Create about.blade.php

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'About - ' . config('app.name', 'SaucePls'))
+
+@section('content')
+    <div class="mx-auto max-w-7xl">
+        <h1 class="text-2xl font-bold text-white">About</h1>
+        <p class="mt-4 text-sm text-gray-400">Coming soon.</p>
+    </div>
+@endsection
